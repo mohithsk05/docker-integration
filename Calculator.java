3ad6d89034b39160a4d94a000720097708001612
mohithsk05 <mohithsk2005@gmail.com>
2026-04-08 18:44:09 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+// CALCULATOR
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        int a = 10, b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+
+        System.out.println("Calculator executed successfully!");
+    }
+}
